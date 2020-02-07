cd1235a55ac2ab146424ba7f0a45537bf4545802
Fabian Wilkens <wilkens.fabian+github.com>
2020-02-07 20:34:09 +0100

    Fix Installation
     Autor:           Fabian Wilkens <wilkens.fabian+github.com>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,8 @@
 # dependencies used by the app
 #pkg_dependencies="ruby ruby-dev rails zlib1g-dev libsqlite3-dev default-libmysqlclient-dev libssl-dev libreadline-dev"
 #pkg_dependencies="                     zlib1g-dev libsqlite3-dev default-libmysqlclient-dev libssl-dev libreadline-dev"
-pkg_dependencies=""
+#pkg_dependencies="libssl-dev libreadline-dev zlib1g-dev"
+pkg_dependencies="git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev"
 
 #=================================================
 # PERSONAL HELPERS
@@ -16,14 +17,14 @@ pkg_dependencies=""
 # Execute a command as another user with login
 # (hence in user home dir, with prior loading of .profile, etc.)
 # usage: exec_login_as USER COMMAND [ARG ...]
-exec_login_as() {
+ynh_exec_login_as() {
 	local user=$1
 	shift 1
 	exec_as $user --login "$@"
 }
 # Execute a command as another user
 # usage: exec_as USER COMMAND [ARG ...]
-exec_as() {
+ynh_exec_as() {
 	local user=$1
 	shift 1
 	
