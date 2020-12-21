c369ecdd7471cead7aaf872f170bb5f89bc1cac7
Fabian Wilkens <wilkens.fabian+github@gmail.com>
2020-12-22 00:03:54 +0100

    Upgrade to 3.5.11, Optimations

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,10 +5,19 @@
 #=================================================
 
 # dependencies used by the app
-#pkg_dependencies="ruby ruby-dev rails zlib1g-dev libsqlite3-dev default-libmysqlclient-dev libssl-dev libreadline-dev"
-#pkg_dependencies="                     zlib1g-dev libsqlite3-dev default-libmysqlclient-dev libssl-dev libreadline-dev"
-#pkg_dependencies="libssl-dev libreadline-dev zlib1g-dev"
-pkg_dependencies="git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev"
+#pkg_dependencies="libsqlite3-dev default-libmysqlclient-dev"
+pkg_dependencies="\
+		zlib1g-dev \
+		libssl-dev \
+		libjemalloc-dev \
+		git \
+		"
+
+RUBY_VERSION="2.7.2"
+NODEJS_VERSION="10"
+
+SOURCE="https://github.com/standardnotes/web"
+COMMIT="31676f1cc61898147b363d442593870ae880d3e5"
 
 #=================================================
 # PERSONAL HELPERS
@@ -17,14 +26,14 @@ pkg_dependencies="git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison 
 # Execute a command as another user with login
 # (hence in user home dir, with prior loading of .profile, etc.)
 # usage: exec_login_as USER COMMAND [ARG ...]
-ynh_exec_login_as() {
+exec_login_as() {
 	local user=$1
 	shift 1
 	exec_as $user --login "$@"
 }
 # Execute a command as another user
 # usage: exec_as USER COMMAND [ARG ...]
-ynh_exec_as() {
+exec_as() {
 	local user=$1
 	shift 1
 	
