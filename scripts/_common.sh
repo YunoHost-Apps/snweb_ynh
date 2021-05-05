4e0e9a35222a06caf753d3e3a97e1009ae53d798
Fabian Wilkens <wilkens.fabian+github@gmail.com>
2021-05-05 20:18:20 +0200

    Switch to ynh_exec_as

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -22,27 +22,6 @@ COMMIT="bc855b0a1722d2f4ac534ea4d4035299acb7a40a"
 # PERSONAL HELPERS
 #=================================================
 
-# Execute a command as another user with login
-# (hence in user home dir, with prior loading of .profile, etc.)
-# usage: exec_login_as USER COMMAND [ARG ...]
-exec_login_as() {
-    local user=$1
-    shift 1
-    exec_as $user --login "$@"
-}
-# Execute a command as another user
-# usage: exec_as USER COMMAND [ARG ...]
-exec_as() {
-    local user=$1
-    shift 1
-
-    if [[ $user = $(whoami) ]]; then
-        eval "$@"
-    else
-        sudo -u "$user" "$@"
-    fi
-}
-
 #================================================
 # EXPERIMENTAL HELPERS
 #=================================================
